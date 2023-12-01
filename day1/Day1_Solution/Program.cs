2edcd875c756a4204acb89711d3d32df4d06b70f
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2023-12-01 23:00:07 +0100

    Cracked part 2 and split the result in two classes

--- day1/Day1_Solution/Program.cs
+++ day1/Day1_Solution/Program.cs
@@ -1,33 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.IO;
- String line;
-try
-{
-    StreamReader sr = new StreamReader("./input.txt");
-    //Read the first line of text
-    line = sr.ReadLine();
 
-    int result = 0;
-    //Continue to read until you reach end of file
-    while (line != null)
-    {
-        string allNumbers = new string(line.Where(c => char.IsDigit(c)).ToArray());
-        string fullNumber = allNumbers[0].ToString();
-        if(allNumbers.Length >= 2){
-            fullNumber +=  allNumbers[allNumbers.Length -1 ].ToString();
-        }else{
-            fullNumber += fullNumber;
-        }
-        result += Int32.Parse(fullNumber);
-        //Read the next line
-        line = sr.ReadLine();
-    }
-    //close the file
-    sr.Close();
-    
-    Console.WriteLine($"The final result is: {result}");
-}
-catch(Exception e)
+
+class Program
 {
-    Console.WriteLine("Exception: " + e.Message);
+    static void Main()
+    {
+        Part1.Result();
+        Part2.Result();
+    }
 }
