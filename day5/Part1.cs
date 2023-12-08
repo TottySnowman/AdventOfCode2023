be5d3d6d0311c318a4abbb49289f1b67ca0282f2
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2023-12-08 08:18:44 +0100

    Modified Day 5 Part 1

--- day5/Part1.cs
+++ day5/Part1.cs
@@ -2,103 +2,48 @@ class Part1
 {
     public static void Result()
     {
-        string[] allLines;
-        long result = 0;
-        List<Dictionary<long, long>> allConversions = new List<Dictionary<long, long>>();
+        var input = File.ReadAllLines("./input.txt");
 
-        try
+        var seeds = input[0].Split(' ').Skip(1).Select(x => long.Parse(x)).ToList();
+        var maps = new List<List<(long from, long to, long adjustment)>>();
+        List<(long from, long to, long adjustment)>? currmap = null;
+        foreach (var line in input.Skip(2))
         {
-            allLines = File.ReadAllLines("./input.txt");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-            return;
-        }
-
-        int startLine = 2;
-        Dictionary<long, long> mappings = new Dictionary<long, long>();
-        List<int> seeds = new List<int>();
-        foreach (String line in allLines)
-        {
-            Console.WriteLine(line);
-            int index = Array.IndexOf(allLines, line);
-            if (String.IsNullOrWhiteSpace(line))
+            if (line.EndsWith(':'))
             {
+                currmap = new List<(long from, long to, long adjustment)>();
+                continue;
+            }
+            else if (line.Length == 0 && currmap != null)
+            {
+                maps.Add(currmap!);
+                currmap = null;
                 continue;
             }
 
-            if (index < startLine)
+            var nums = line.Split(' ').Select(x => long.Parse(x)).ToArray();
+            currmap!.Add((nums[1], nums[1] + nums[2] - 1, nums[0] - nums[1]));
+        }
+        if (currmap != null)
+            maps.Add(currmap);
+
+        // Part 1
+        var result = long.MaxValue;
+        foreach (var seed in seeds)
+        {
+            var value = seed;
+            foreach (var map in maps)
             {
-                if (index == 0)
+                foreach (var item in map)
                 {
-                    //Reading Seeds
-                    String[] seedSplit = line.Split(":");
-                    String[] allSeeds = seedSplit[1].Split(" ");
-                    foreach (string seed in allSeeds)
+                    if (value >= item.from && value <= item.to)
                     {
-                        try
-                        {
-                            seeds.Add(int.Parse(seed));
-                        }
-                        catch (Exception e)
-                        {
-
-                        }
-
+                        value += item.adjustment;
+                        break;
                     }
                 }
-                continue;
             }
-
-            if (line.Contains("map"))
-            {
-                //Add the dictonary before to the list and create a new dictonary
-                if (mappings.Count > 0)
-                {
-                    allConversions.Add(mappings);
-                }
-                mappings = new Dictionary<long, long>();
-                Console.WriteLine("Map: " + line);
-                continue;
-            }
-
-            String[] lineSplit = line.Split(" ");
-            long rsltStart = long.Parse(lineSplit[0]);
-            long srcStart = long.Parse(lineSplit[1]);
-            long range = long.Parse(lineSplit[2]);
-
-            for (long x = srcStart; x < srcStart + range; x++)
-            {
-                mappings.Add(x, rsltStart);
-                rsltStart++;
-            }
-            Console.WriteLine("One line finished!");
-        }
-
-
-        allConversions.Add(mappings);
-        Console.WriteLine($"All mappings are done! In total there are {allConversions.Count}");
-
-        foreach (long seed in seeds)
-        {
-            long res = seed;
-            foreach (var dic in allConversions)
-            {
-                if (dic.TryGetValue(res, out long foundValue))
-                {
-                    res = foundValue;
-                }
-            }
-
-            if (result == 0)
-            {
-                result = res;
-            }
-            if (res < result)
-            {
-                result = res;
-            }
+            result = Math.Min(result, value);
         }
 
         Console.WriteLine($"The lowest Location is: {result}");
--- day5/Program.cs
+++ day5/Program.cs
@@ -1,99 +1,9 @@
 ﻿
-using System.Diagnostics;
 class Program
 {
     static void Main()
     {
         Part1.Result();
         Part2.Result();
-        /* var input = File.ReadAllLines("./input.txt");
-
-        var watch = Stopwatch.StartNew();
-
-        var seeds = input[0].Split(' ').Skip(1).Select(x => long.Parse(x)).ToList();
-        var maps = new List<List<(long from, long to, long adjustment)>>();
-        List<(long from, long to, long adjustment)>? currmap = null;
-        foreach (var line in input.Skip(2))
-        {
-            if (line.EndsWith(':'))
-            {
-                currmap = new List<(long from, long to, long adjustment)>();
-                continue;
-            }
-            else if (line.Length == 0 && currmap != null)
-            {
-                maps.Add(currmap!);
-                currmap = null;
-                continue;
-            }
-
-            var nums = line.Split(' ').Select(x => long.Parse(x)).ToArray();
-            currmap!.Add((nums[1], nums[1] + nums[2] - 1, nums[0] - nums[1]));
-        }
-        if (currmap != null)
-            maps.Add(currmap);
-
-        // Part 1
-        var result1 = long.MaxValue;
-        foreach (var seed in seeds)
-        {
-            var value = seed;
-            foreach (var map in maps)
-            {
-                foreach (var item in map)
-                {
-                    if (value >= item.from && value <= item.to)
-                    {
-                        value += item.adjustment;
-                        break;
-                    }
-                }
-            }
-            result1 = Math.Min(result1, value);
-        }
-
-        // Part 2
-        var ranges = new List<(long from, long to)>();
-        for (int i = 0; i < seeds.Count; i += 2)
-            ranges.Add((from: seeds[i], to: seeds[i] + seeds[i + 1] - 1));
-
-        foreach (var map in maps)
-        {
-            var orderedmap = map.OrderBy(x => x.from).ToList();
-
-            var newranges = new List<(long from, long to)>();
-            foreach (var r in ranges)
-            {
-                var range = r;
-                foreach (var mapping in orderedmap)
-                {
-                    if (range.from < mapping.from)
-                    {
-                        newranges.Add((range.from, Math.Min(range.to, mapping.from - 1)));
-                        range.from = mapping.from;
-                        if (range.from > range.to)
-                            break;
-                    }
-
-                    if (range.from <= mapping.to)
-                    {
-                        newranges.Add((range.from + mapping.adjustment, Math.Min(range.to, mapping.to) + mapping.adjustment));
-                        range.from = mapping.to + 1;
-                        if (range.from > range.to)
-                            break;
-                    }
-                }
-                if (range.from <= range.to)
-                    newranges.Add(range);
-            }
-            ranges = newranges;
-        }
-        var result2 = ranges.Min(r => r.from);
-
-     watch.Stop(); */
-
-        /*         Console.WriteLine($"Result1 = {result1}");
-                Console.WriteLine($"Result2 = {result2}");
-                Console.WriteLine($"Took = {watch.ElapsedMilliseconds}ms"); */
     }
 }
